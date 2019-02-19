245faba85b1711d97bd3b7728057f00a2ca2f7a1
wildmolasses <changes@gmail.com>
2019-02-18 22:53:40 -0700

    moving towards chifra, forgot to add file to staging

--- build/src/server.js
+++ build/src/server.js
@@ -6,9 +6,9 @@ const port = 80;
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 
-app.get('/address/:address', (req, res) => {
+app.get('/txIndex', (req, res) => {
     
-    if (req.params.address.length != 42)
+    if (req.query.address.length != 42)
         return res.send({status: "err", message: "Expecting an Ethereum address 42 characters long."});
   
     let acctScrape = spawn("acctScrape", ['--list', '--nocolor', req.params.address]);
@@ -28,8 +28,27 @@ app.get('/address/:address', (req, res) => {
         console.log(`child process exited with code ${code}`);
         return res.end();
     })
-
 })
+
+app.post('/init', (req, res) => {
+    
+    if (!req.query.address) return res.send({status: "err", message: "No address provided."});
+    if (req.query.address.length != 42) return res.send({status: "err", message: "Expecting an Ethereum address 42 characters long."});
+
+    let chifra = spawn("chifra", ['init', req.query.address]);
+
+    chifra.stdout.on('data', (chunk) => {
+        return res.write(chunk);
+    });
+
+    chifra.on('exit', (code) => {
+        console.log(`exiting: ${code}`);
+        console.log(`child process exited with code ${code}`);
+        return res.end();
+    })
+})
+
+// app.get('/export')
 
 app.listen(port, () => {
     console.log('We are live on ' + port);
