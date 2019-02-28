82f8d63466976c34f5135bc6dcdd60d28d41a3cd
Thomas Jay Rush <jrush@quickblocks.io>
2019-02-27 20:47:06 -0500

    Trying to get the seed data into the docker image

--- build/trueblocks.entrypoint.sh
+++ build/trueblocks.entrypoint.sh
@@ -2,8 +2,19 @@
 
 echo "setting rpc provider to $RPC_PROVIDER"
 sed -i "s|\"http:\/\/.*\"|\"$RPC_PROVIDER\"|" /root/.quickBlocks/quickBlocks.toml
-cat /root/.quickBlocks/quickBlocks.toml
-cd /root/.quickBlocks/monitors
+
+echo "==================== checking install ============================="
+cat /root/.quickBlocks/quickBlocks.toml | grep -v "^$"
+which chifra
+which acctScrape
+which blockScrape
+which grabABI
+
+echo "==================== starting processes ============================="
+# Run the webserver
 forever /root/api/server.js &
+
+# We need to wait until the seed is done
+chifra seed
 chifra scrape &
 chifra daemon
