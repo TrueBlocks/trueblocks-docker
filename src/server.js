2cdde66c40e4af339ed1212c94c79a7a60bbc9e3
wildmolasses <changes@gmail.com>
2019-04-22 19:55:43 +0900

    api: simple debug flag

--- src/server.js
+++ src/server.js
@@ -2,9 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {spawn} = require('child_process');
 const app = express();
-const port = 80;
+const port = 8080;
+let debug = false;
+let env = Object.create( process.env );
 app.use(bodyParser.json());
 app.use(bodyParser.text());
+
+const checkDebug = (req) => {
+    console.log(req.query.debug)
+    if(req.query.debug === undefined)
+        return false;
+    env.TEST_MODE = true;
+    return true;
+}
 
 app.get('/', (req, res) => {
     return res.send(`Welcome to TrueBlocks!\n
@@ -14,11 +24,20 @@ app.get('/', (req, res) => {
 })
 
 app.get('/list', (req, res) => {
+    debug = !debug && checkDebug(req);
+   
+    debugMsg = {
+        received: debug && res.write("Got your request.\n"),
+        started: debug && res.write("Starting chifra list...\n"),
+    }
 
+    debugMsg.received;
     if (req.query.address.length != 42)
         return res.send({status: "err", message: "Expecting an Ethereum address 42 characters long.\n"});
 
-    let chifra = spawn("chifra", ['list', req.query.address, '--nocolor']);
+    debugMsg.started;
+    let chifra = spawn("chifra", ['list', req.query.address, '--nocolor'],  {env: env});
+
     chifra.stdout.pipe(res).on('finish', (code) => {
         console.log(`"chifra list" exiting: ${code}`);
         console.log(`child process exited with code ${code}`);
@@ -28,11 +47,19 @@ app.get('/list', (req, res) => {
 })
 
 app.get('/export', (req, res) => {
-
+    debug = !debug && checkDebug(req);
+   
+    debugMsg = {
+        received: debug && res.write("Got your request.\n"),
+        started: debug && res.write("Starting chifra export...\n"),
+    }
+    
+    debugMsg.received;
     if (req.query.address.length != 42)
         return res.send({status: "err", message: "Expecting an Ethereum address 42 characters long.\n"});
 
-    let chifra = spawn("chifra", ['export', req.query.address, '--nocolor']);
+    let chifra = spawn("chifra", ['export', req.query.address, '--nocolor'], {env: env});
+    debugMsg.started;
     chifra.stdout.pipe(res).on('finish', (code) => {
         console.log(`"chifra export" exiting: ${code}`);
         console.log(`child process exited with code ${code}`);
@@ -43,11 +70,20 @@ app.get('/export', (req, res) => {
 
 app.get('/ls', (req, res) => {
 
+    debug = !debug && checkDebug(req);
+   
+    debugMsg = {
+        received: debug && res.write("Got your request.\n"),
+        started: debug && res.write("Starting chifra ls...\n"),
+    }
+
     var longList = ""
     if (req.query.ll)
         longList = "-l";
-
-    let chifra = spawn("chifra", ['ls', '--nocolor', longList]);
+    debugMsg.received;
+    let chifra = spawn("chifra", ['ls', '--nocolor', longList], {env: env});
+    
+    debugMsg.started;
     chifra.stdout.pipe(res).on('finish', (code) => {
         console.log(`"chifra ls" exiting: ${code}`);
         console.log(`child process exited with code ${code}`);
