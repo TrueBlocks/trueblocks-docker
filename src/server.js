8b529a49149f5bb66076e02ab94184fccb865d68
Thomas Jay Rush <jrush@quickblocks.io>
2019-04-25 07:19:12 -0400

    Removed debugging stuff (sorry) and added a few parameters to chifra calls

--- src/server.js
+++ src/server.js
@@ -21,72 +21,51 @@ app.get('/', (req, res) => {
     return res.send(
 `Welcome to TrueBlocks!
 Try one of the following:
-    /list?address=0x5894110995b8c8401bd38262ba0c8ee41d4e4658
-    /export?address=0x5894110995b8c8401bd38262ba0c8ee41d4e4658
+    /list?address=0x8ad69ae99804935d56704162e3f6a6f442d2ed4a
+    /export?address=0x8ad69ae99804935d56704162e3f6a6f442d2ed4a
 `);
 })
 
 app.get('/list', (req, res) => {
     debug = !debug && checkDebug(req,res);
-   
-    debugMsg = {
-        received: debug && res.write("Got your request.\n"),
-        started: debug && res.write("Starting chifra list...\n"),
-    }
 
-    debugMsg.received;
     if (req.query.address.length != 42)
-        return res.send({status: "err", message: "Expecting an Ethereum address 42 characters long.\n"});
+        return res.send({status: "err", message: 'Expecting an Ethereum address 42 characters long.' });
 
-    debugMsg.started;
-    let chifra = spawn("chifra", ['list', req.query.address, '--nocolor'],  {env: env});
-
+    let chifra = spawn("chifra", ['list', req.query.address, '--useBlooms', '--nocolor'],  {env: env});
     chifra.stdout.pipe(res).on('finish', (code) => {
         console.log(`"chifra list" exiting: ${code}`);
         console.log(`child process exited with code ${code}`);
         return res.end();
     })
-
 })
 
 app.get('/export', (req, res) => {
     debug = !debug && checkDebug(req,res);
-   
-    debugMsg = {
-        received: debug && res.write("Got your request.\n"),
-        started: debug && res.write("Starting chifra export...\n"),
-    }
-    
-    debugMsg.received;
-    if (req.query.address.length != 42)
-        return res.send({status: "err", message: "Expecting an Ethereum address 42 characters long.\n"});
 
+    if (req.query.address.length != 42)
+        return res.send({status: "err", message: `Expecting an Ethereum address 42 characters long.`});
     let chifra = spawn("chifra", ['export', req.query.address, '--nocolor'], {env: env});
-    debugMsg.started;
+
     chifra.stdout.pipe(res).on('finish', (code) => {
         console.log(`"chifra export" exiting: ${code}`);
         console.log(`child process exited with code ${code}`);
         return res.end();
     })
-
 })
 
 app.get('/ls', (req, res) => {
-
     debug = !debug && checkDebug(req,res);
-   
-    debugMsg = {
-        received: debug && res.write("Got your request.\n"),
-        started: debug && res.write("Starting chifra ls...\n"),
-    }
 
     var longList = ""
     if (req.query.ll)
         longList = "-l";
-    debugMsg.received;
-    let chifra = spawn("chifra", ['ls', '--nocolor', longList], {env: env});
-    
-    debugMsg.started;
+
+// address is optional don't know how to test
+//    if (req.query.address.length != 42)
+//        return res.send({status: "err", message: `Expecting an Ethereum address 42 characters long.`});
+    let chifra = spawn("chifra", ['ls', '--nocolor', req.query.address, longList], {env: env});
+
     chifra.stdout.pipe(res).on('finish', (code) => {
         console.log(`"chifra ls" exiting: ${code}`);
         console.log(`child process exited with code ${code}`);
