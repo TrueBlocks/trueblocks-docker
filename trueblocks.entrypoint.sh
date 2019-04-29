dda3892cdbedfcba34c7cae173620371b609a3d2
wildmolasses <changes@gmail.com>
2019-04-29 21:39:16 +0900

    entrypoint: set api to port 80

--- trueblocks.entrypoint.sh
+++ trueblocks.entrypoint.sh
@@ -4,5 +4,5 @@ echo "setting rpc provider to $RPC_PROVIDER"
 sed -i "s|\"http:\/\/.*\"|\"$RPC_PROVIDER\"|" /root/.quickBlocks/quickBlocks.toml
 cat /root/.quickBlocks/quickBlocks.toml
 cd /root/.quickBlocks/monitors
-forever /root/api/server.js &
+forever /root/api/server.js 80 &
 chifra scrape --sleep 2
