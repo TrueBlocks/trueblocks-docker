6659cdc8f0b726a908aa9f8e4b3382c1c9bf1428
Thomas Jay Rush <jrush@quickblocks.io>
2019-07-11 11:09:26 -0400

    Squash of squash: Adding new routes, updating existing route to work with new trueblocks-core command lines (#77)
    * Adding new routes, updating existing route to work with new trueblocks-core command lines
    
    * Squash of Jay's PR: Fixing broken routes (#79)
    
    * Fixing broken routes
    
    * Updating
    
    * Added list2, messages, export2, and balances routes
    
    * Cleaning up server code. Adds all_logs (unimplemented in chifra) and tracecnt -- avoid loading too many traces
    
    * Adding export_logs route, removing all_logs route
    
    * Updating .gitignore

--- src/server.js
+++ src/server.js
@@ -5,8 +5,11 @@ const app = express();
 const port = !isNaN(process.argv[2]) ? process.argv[2] : 8080;
 let env = process.env;
 env.API_MODE = true;
+//env.TEST_MODE = true;
 app.use(bodyParser.json());
 app.use(bodyParser.text());
+
+//var X = import("0x509fa8d8b2c7962bd9a8c32fb79dbdecf81b312b011be240903ccea3410f22a1.json")
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -20,6 +23,23 @@ app.get('/', (req, res) => {
 Try one of the following:
     /list?address=0x8ad69ae99804935d56704162e3f6a6f442d2ed4a
     /export?address=0x8ad69ae99804935d56704162e3f6a6f442d2ed4a
+    /export_logs/:id
+    /ls
+    /accounts/:id
+    /blocks/:id
+    /transactions/:id
+    /logs/:id
+    /receipts/:id
+    /traces/:id
+    /tracecnt/:id
+    /abi/:id
+    /state/balance/:id
+    /state/code/:id
+    /state/nonce/:id
+    /balances/:id
+    /message/:bytes
+    /slurp/:id
+    /quotes/:id
 `);
 })
 
@@ -27,7 +47,7 @@ var debug = "--verbose:6"
 var cnt = 0;
 function reportAndSend(routeName, code, res) {
     console.log(`"${routeName}" exiting: ${code === undefined ? "OK" : code}`);
-    console.log(`------------- ${++cnt} ---------------------------`)
+    console.log(`------------- ${++cnt} ---------------------------`);
     return res.send();
 }
 //debug = ""
@@ -42,14 +62,45 @@ app.get('/export', (req, res) => {
     })
 })
 
+app.get('/export2', (req, res) => {
+    if (req.query.address.length != 42)
+        return res.send({status: "err", message: `Expecting an Ethereum address 42 characters long.`});
+    let chifra = spawn("chifra", ['export', req.query.address, '--fmt', 'txt', '--to_file', '--nocolor'], {env: env});
+    chifra.stderr.pipe(process.stderr);
+    chifra.stdout.pipe(res).on('finish', (code) => {
+        reportAndSend("export2", code, res);
+    })
+})
+
+app.get('/export_logs/:id', (req, res) => {
+    var id = "";
+    if (typeof req.params.id != undefined)
+        id = req.params.id;
+    let chifra = spawn("chifra", ['export', '--logs', `${id}`, '--fmt', 'txt', '--to_file', debug, '--nocolor'], { env: env });
+    chifra.stderr.pipe(process.stderr);
+    chifra.stdout.pipe(res).on('finish', (code) => {
+        reportAndSend("logs", code, res);
+    })
+})
+
 app.get('/list', (req, res) => {
     if (req.query.address.length != 42)
         return res.send({status: "err", message: 'Expecting an Ethereum address 42 characters long.' });
-
-    let chifra = spawn("chifra", ['list', req.query.address, '--useBlooms', debug, '--nocolor'],  {env: env});
+    let chifra = spawn("chifra", ['list', req.query.address, debug, '--nocolor'],  {env: env});
     chifra.stderr.pipe(process.stderr);
     chifra.stdout.pipe(res).on('finish', (code) => {
         reportAndSend("list", code, res);
+    })
+})
+
+app.get('/list2/:id', (req, res) => {
+    var id = "";
+    if (typeof req.params.id != undefined)
+        id = req.params.id;
+    let chifra = spawn("chifra", ['list', `${id}`, '--fmt', 'txt', '--to_file', debug, '--nocolor'],  {env: env});
+    chifra.stderr.pipe(process.stderr);
+    chifra.stdout.pipe(res).on('finish', (code) => {
+        reportAndSend("list2", code, res);
     })
 })
 
@@ -90,10 +141,11 @@ app.get('/transactions/:id', (req, res) => {
     var id = "";
     if (typeof req.params.id != undefined)
         id = req.params.id;
-    let chifra = spawn("chifra", ['data', '--trans', `${id}`, debug, '--nocolor'], { env: env });
+    console.log(req.params.id);
+    let chifra = spawn("chifra", ['data', '--trans', `${id}`, '--trace', '--articulate', '--fmt', 'json', debug, '--nocolor'], { env: env });
     chifra.stderr.pipe(process.stderr);
     chifra.stdout.pipe(res).on('finish', (code) => {
-        reportAndSend("trans", code, res);
+        reportAndSend("transactions", code, res);
     })
 })
 
@@ -101,7 +153,7 @@ app.get('/logs/:id', (req, res) => {
     var id = "";
     if (typeof req.params.id != undefined)
         id = req.params.id;
-    let chifra = spawn("chifra", ['data', '--logs', `${id}`, debug, '--nocolor'], { env: env });
+    let chifra = spawn("chifra", ['data', '--logs', `${id}`, '--articulate', '--fmt', 'txt', '--to_file', debug, '--nocolor'], { env: env });
     chifra.stderr.pipe(process.stderr);
     chifra.stdout.pipe(res).on('finish', (code) => {
         reportAndSend("logs", code, res);
@@ -123,10 +175,21 @@ app.get('/traces/:id', (req, res) => {
     var id = "";
     if (typeof req.params.id != undefined)
         id = req.params.id;
-    let chifra = spawn("chifra", ['data', '--traces', `${id}`, debug, '--nocolor'], { env: env });
+    let chifra = spawn("chifra", ['data', '--traces', `${id}`, '--articulate', '--fmt', 'csv', '--to_file', debug, '--nocolor'], { env: env });
     chifra.stderr.pipe(process.stderr);
     chifra.stdout.pipe(res).on('finish', (code) => {
         reportAndSend("traces", code, res);
+    })
+})
+
+app.get('/tracecnt/:id', (req, res) => {
+    var id = "";
+    if (typeof req.params.id != undefined)
+        id = req.params.id;
+    let chifra = spawn("chifra", ['data', '--traces', `${id}`, '--noHeader', '--countOnly', '--fmt', 'txt', debug, '--nocolor'], { env: env });
+    chifra.stderr.pipe(process.stderr);
+    chifra.stdout.pipe(res).on('finish', (code) => {
+        reportAndSend("tracecnt", code, res);
     })
 })
 
@@ -141,6 +204,73 @@ app.get('/slurp/:id', (req, res) => {
     })
 })
 
+app.get('/abi/:id', (req, res) => {
+    var id = "";
+    if (typeof req.params.id != undefined)
+        id = req.params.id;
+    let chifra = spawn("chifra", ['data', '--abi', `${id}`, debug, '--nocolor'], { env: env });
+    chifra.stderr.pipe(process.stderr);
+    chifra.stdout.pipe(res).on('finish', (code) => {
+        reportAndSend("abi", code, res);
+    })
+})
+
+app.get('/state/balance/:id', (req, res) => {
+    var id = "";
+    if (typeof req.params.id != undefined)
+        id = req.params.id;
+    let chifra = spawn("chifra", ['data', '--balance', '--mode', 'some', `${id}`, debug, '--nocolor'], { env: env });
+    chifra.stderr.pipe(process.stderr);
+    chifra.stdout.pipe(res).on('finish', (code) => {
+        reportAndSend("state/balance", code, res);
+    })
+})
+
+app.get('/state/code/:id', (req, res) => {
+    var id = "";
+    if (typeof req.params.id != undefined)
+        id = req.params.id;
+    let chifra = spawn("chifra", ['data', '--code', '--mode', 'some', `${id}`, debug, '--nocolor'], { env: env });
+    chifra.stderr.pipe(process.stderr);
+    chifra.stdout.pipe(res).on('finish', (code) => {
+        reportAndSend("state/code", code, res);
+    })
+})
+
+app.get('/state/nonce/:id', (req, res) => {
+    var id = "";
+    if (typeof req.params.id != undefined)
+        id = req.params.id;
+    let chifra = spawn("chifra", ['data', '--nonce', '--mode', 'some', `${id}`, debug, '--nocolor'], { env: env });
+    chifra.stderr.pipe(process.stderr);
+    chifra.stdout.pipe(res).on('finish', (code) => {
+        reportAndSend("state/nonce", code, res);
+    })
+})
+
+app.get('/balances/:id', (req, res) => {
+    var id = "";
+    if (typeof req.params.id != undefined)
+        id = req.params.id;
+    let chifra = spawn("chifra", ['balances', `${id}`, debug, '--nocolor'], { env: env });
+    chifra.stderr.pipe(process.stderr);
+    chifra.stdout.pipe(res).on('finish', (code) => {
+        reportAndSend("balances", code, res);
+    })
+})
+
+app.get('/message/:id', (req, res) => {
+    var id = "";
+    if (typeof req.params.id != undefined)
+        id = req.params.id;
+//    let chifra = spawn("chifra", ['data', '--message', `${id}`, debug, '--nocolor'], { env: env });
+    let chifra = spawn("chifra", ['data', '--message', `${id}`, '--nocolor'], { env: env });
+    chifra.stderr.pipe(process.stderr);
+    chifra.stdout.pipe(res).on('finish', (code) => {
+        reportAndSend("message", code, res);
+    })
+})
+
 app.get('/quotes/:id', (req, res) => {
     var id = "";
     if (typeof req.params.id != undefined)
@@ -148,18 +278,10 @@ app.get('/quotes/:id', (req, res) => {
     let chifra = spawn("chifra", ['data', '--quotes', `${id}`, debug, '--nocolor'], { env: env });
     chifra.stderr.pipe(process.stderr);
     chifra.stdout.pipe(res).on('finish', (code) => {
-        reportAndSend("slurp", code, res);
-    })
-})
-
-app.get('/functions', (req, res) => {
-    let chifra = spawn("chifra", ['functions', '0xfb6916095ca1df60bb79ce92ce3ea74c37c5d359', debug, '--nocolor'], { env: env });
-    chifra.stderr.pipe(process.stderr);
-    chifra.stdout.pipe(res).on('finish', (code) => {
-        reportAndSend("functions", code, res);
+        reportAndSend("quotes", code, res);
     })
 })
 
 app.listen(port, () => {
-    console.log('TrueBlocks API initialized on port ' + port);
+    console.log('TrueBlocks Data API initialized on port ' + port);
 });
