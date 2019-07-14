73a61d8294809656e315fe784f45ea5a7f8efc3b
Thomas Jay Rush <jrush@quickblocks.io>
2019-07-14 07:37:10 -0400

    Remove unneeded cd in monitors folder. Export DOCKER_MODE environment so tools are aware.

--- trueblocks.entrypoint.sh
+++ trueblocks.entrypoint.sh
@@ -3,6 +3,6 @@
 echo "setting rpc provider to $RPC_PROVIDER"
 sed -i "s|\"http:\/\/.*\"|\"$RPC_PROVIDER\"|" /root/.quickBlocks/quickBlocks.toml
 cat /root/.quickBlocks/quickBlocks.toml
-cd /root/.quickBlocks/monitors
+export DOCKER_MODE=true
 forever /root/api/server.js 80 &
-chifra scrape --sleep 2
+chifra scrape
