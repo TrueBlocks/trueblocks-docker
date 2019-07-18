828fe76abff1da5a65b3565799a643531bda608e
wildmolasses <changes@gmail.com>
2019-07-18 16:52:40 -0400

    cleanup

--- param-parser/index.js
+++ param-parser/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const util = require('util');
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
-
+const groupBy = require('./helpers/groupBy').groupBy;
 const blueprintReplace = require('./replacers/blueprintReplace').blueprintReplace;
 const routeToToolMap = require('./routeToToolMap.json');
 const debug = true;
@@ -24,10 +24,10 @@ stdin.on('data', (chunk) => {
   data += chunk;
 });
 
-stdin.on('end', async () => {
+const parseInput = async (data) => {
   let rx = /.*\/(.*)\/options.cpp.*\(\"(.*)\"\,.*\"(.*)\"\)\,/;
   let lines = data.split("\n");
-  let reglines = lines.map(line => {
+  let parsedLines = lines.map(line => {
       line = rx.exec(line);
       if (line === null)
         return null;
@@ -47,22 +47,14 @@ stdin.on('end', async () => {
   
     if(debug) {
       // console.log(reglines.map(line => line.option));
-      await writeFile("params.txt", reglines.map(line => line.option).join('\n'));
+      await writeFile("params.txt", parsedLines.map(line => line.option).join('\n'));
     }
-    
-  
-    var groupBy = function(xs, key) {
-      return xs.reduce(function(rv, x) {
-        (rv[x[key]] = rv[x[key]] || []).push(x);
-        return rv;
-      }, {});
-    };
-
-    reglines = groupBy(reglines, 'tool')
-
-  // console.log(reglines);
+    return groupBy(parsedLines, 'tool');
+}
+stdin.on('end', async () => {
+  let parsedInput = await parseInput(data);
   try {
-  await jobs[0].do(jobs[0].templateFilepath, jobs[0].outputFilepath, reglines, routeToToolMap);
+    await jobs[0].do(jobs[0].templateFilepath, jobs[0].outputFilepath, parsedInput, routeToToolMap);
   } catch {(e) => {
     console.log(e);
   }}
--- param-parser/replacers/blueprintReplace.js
+++ param-parser/replacers/blueprintReplace.js
@@ -4,36 +4,40 @@ const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
 module.exports.blueprintReplace = async (templateFilepath, outputFilepath, data, routeToToolMap) => {
-  
-    let replacer = (match, type, routeName) => {  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#Specifying_a_function_as_a_parameter
-      if(routeToToolMap[routeName] === undefined) 
-      throw(`ERROR: no mapping for ${routeName} in the route to tool map.`);
-      let params = routeToToolMap[routeName]
-        .map(toolName => data[toolName]
-          .filter(param => param.option != '') // no empty parameter names. these aren't parameters, they are tool description.
-          ) 
-        .reduce((acc, val) => acc.concat(val), []); // flatten
-      
-      if(type === "URI") {
-        let paramsFormatted = params.map(param => {
-          return `{?${param.option}}`
-        }).join("");
-        return `/${routeName}${paramsFormatted}`;
-      }
-  
-      else if(type === "PARAMS") {  
-        let paramsFormatted = params.map(param => {
-          param.exampleData = '';
-          return `    + ${param.option}: ${param.exampleData} (${param.isRequired ? "required" : "optional"}, ${param.type}) - ${param.desc}`
-        }).join("\n");
-        return paramsFormatted;
-      }
-    }
+
+  let replacer = (match, type, routeName) => {  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#Specifying_a_function_as_a_parameter
     
+    console.log(`routeName: ${routeName}, ${routeToToolMap[routeName]}`);
+    if(routeToToolMap[routeName] === undefined) 
+      throw(`ERROR: no mapping for ${routeName} in the route to tool map.`);
+    let params = routeToToolMap[routeName]
+      .map(toolName => data[toolName]
+        .filter(param => param.option != '') // no empty parameter names. these aren't parameters, they are tool description.
+        ) 
+      .reduce((acc, val) => acc.concat(val), []); // flatten
+    
+    // format for GENERATE:URI
+    if(type === "URI") {
+      let paramsFormatted = params.map(param => {
+        return `{?${param.option}}`
+      }).join("");
+      return `/${routeName}${paramsFormatted}`;
+    }
+
+    // format for GENERATE:PARAMS
+    else if(type === "PARAMS") {  
+      let paramsFormatted = params.map(param => {
+        param.exampleData = '';
+        return `    + ${param.option}: ${param.exampleData} (${param.isRequired ? "required" : "optional"}, ${param.type}) - ${param.desc}`
+      }).join("\n");
+      return paramsFormatted;
+    }
+  }
+
     try {
       let template = await readFile(templateFilepath);
       template = template.toString();
-      let rx = /\<\<GENERATE:(.*):(.*)\>\>/g;
+      let rx = /\<\<GENERATE:(.*):(.*)\>\>/g;      
       let result = template.replace(rx, replacer);
       await writeFile(outputFilepath, result);
       console.log(`Generated output written to ${outputFilepath}`);
