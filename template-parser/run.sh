5b93315be222ea6a45abd0f96a5dda09b5a5693d
wildmolasses <changes@gmail.com>
2019-07-25 16:05:54 -0700

    parses from csv now

--- template-parser/run.sh
+++ template-parser/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 npm install
-find ../../trueblocks-core/src/. -name "*.cpp" -exec grep -His "COption(" {} \; | node index.js
+cat misc/example.csv | node index.js
 node ./node_modules/snowboard/lib/main.js html -o ./output/docs.html ./output/apiary.generated.apib
 cp ./output/docs.html ../api/docs/index.html
 cp ./output/server.generated.js ../api/server.generated.js
