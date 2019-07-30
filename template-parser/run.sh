8077bda051ac4b2512d55b47b1afc263f42751f8
wildmolasses <changes@gmail.com>
2019-07-30 19:05:02 -0400

    update run.sh

--- template-parser/run.sh
+++ template-parser/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 npm install
-cat misc/example.csv | node index.js
+node index.js -i ./misc/example.csv
 node ./node_modules/snowboard/lib/main.js html -o ./output/docs.html ./output/apiary.generated.apib
 cp ./output/docs.html ../api/docs/index.html
-cp ./output/server.generated.js ../api/server.generated.js
+cp ./output/apiOptions.generated.json ../api/apiOptions.generated.json
