7bff235031164103550f9573d9bd8f4feaad4c62
wildmolasses <changes@gmail.com>
2019-08-05 13:44:30 -0400

    improve entrypoint sed replace

--- trueblocks.entrypoint.sh
+++ trueblocks.entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "setting rpc provider to $RPC_PROVIDER"
-sed -i "s|\"http:\/\/.*\"|\"$RPC_PROVIDER\"|" /root/.quickBlocks/quickBlocks.toml
+sed -i "s|rpcProvider =.*|rpcProvider = \"$RPC_PROVIDER\"|" /root/.quickBlocks/quickBlocks.toml
 cat /root/.quickBlocks/quickBlocks.toml
 
 export DOCKER_MODE=true
