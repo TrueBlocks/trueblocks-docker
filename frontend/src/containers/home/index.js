e5d04da417ea54c512489e1de25c8eb184dece56
wildmolasses <changes@gmail.com>
2019-08-08 19:09:37 -0400

    start styling

--- frontend/src/containers/home/index.js
+++ frontend/src/containers/home/index.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import { push } from 'connected-react-router'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
 import SystemStatus from './system-status'
 
 const Home = props => (
--- frontend/src/containers/home/system-status.js
+++ frontend/src/containers/home/system-status.js
@@ -9,7 +9,7 @@ import {withPolling} from "../../modules/withPolling"
 
 const SystemStatus = (props) => {
     return (
-        <div>
+        <div className="system-status">
             <SystemDetails {...props}/>
             <button onClick={props.changePage}>
             Settings
@@ -25,23 +25,26 @@ class SystemDetails extends React.Component {
    render = () => {
     console.log(this.props.systemData.is_scraping)
     if(this.props.systemData.is_scraping !== undefined) {
-        return (<div>
-        <h1>System Status</h1>
-        <p>API status: {`${this.props.systemData.is_scraping}`}</p>
-        <p>Scraping: true</p>
-        <p>Block Number: 7241000</p>
-        <p>Head of chain: 7500000</p>
-        <p>Api endpoint: localhost:8080</p>
+        return (<div className="system-details">
+        <div className="item">API status:</div>
+        <div className="item">{`${this.props.systemData.is_scraping}`}</div>
+        <div className="item">Scraping:</div>
+        <div className="item">true</div>
+        <div className="item">Block Number:</div>
+        <div className="item">7241000</div>
+        <div className="item">Head of chain:</div>
+        <div className="item">7500000</div>
+        {/* <p>Api endpoint: localhost:8080</p>
         <p>Client version: Trueblocks v0.7</p>
-        <p>Disk size: 9GB</p>
+        <p>Disk size: 9GB</p> */}
     
-        <p>Node status: {this.props.systemData.is_scraping}</p>
+        {/* <p>Node status: {this.props.systemData.is_scraping}</p>
         <p>Scraping: true</p>
         <p>Block Number: 7241000</p>
         <p>Head of chain: 7500000</p>
         <p>Api endpoint: localhost:8080</p>
         <p>Client version: Trueblocks v0.7</p>
-        <p>Disk size: 9GB</p>
+        <p>Disk size: 9GB</p> */}
       </div>)
     }
     return (<div>Not connected</div>)
