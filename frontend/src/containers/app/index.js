3974d58ca51330e0fe4de8f83c3cb61925394b76
wildmolasses <changes@gmail.com>
2019-08-09 14:07:01 -0400

    add some stuff

--- frontend/src/containers/app/index.js
+++ frontend/src/containers/app/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Link } from 'react-router-dom'
 import Home from '../home'
 import About from '../about'
+import Documentation from '../documentation'
 import Settings from '../settings'
 
 const App = () => (
@@ -9,11 +10,14 @@ const App = () => (
     <header>
       <Link to="/">Home</Link>
       <Link to="/about-us">About</Link>
+      <Link to="/docs">Documentation</Link>
+      <Link to="/settings">Settings</Link>
     </header>
 
     <main>
       <Route exact path="/" component={Home} />
       <Route exact path="/about-us" component={About} />
+      <Route exact path="/docs" component={Documentation} />
       <Route exact path="/settings" component={Settings} />
     </main>
   </div>
---
+++ frontend/src/containers/documentation/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Documentation = props => (
+  <div className="docs">
+      hey
+  </div>
+)
+
+export default Documentation;
