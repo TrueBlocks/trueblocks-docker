8e7ecd41aea28b764281b403493d24181bb821e0
wildmolasses <changes@gmail.com>
2019-08-17 14:49:11 +0200

    code generator: tighten the screws around new csv format

--- template-parser/components/inputParser.js
+++ template-parser/components/inputParser.js
@@ -7,10 +7,8 @@ const utils = require('./utils');
 const inputParser = async (data) => {
   let lines = parse(data, {delimiter: ',', columns: true});
   let parsedLines = lines.map(item => {
-      item.api_enabled = item.api_enabled === "TRUE" ? true : false;
-      item.api_required = item.api_required === "TRUE" ? true : false;
+      item.is_required = item.is_required === "TRUE" ? true : false;
       item.api_visible = item.api_visible === "TRUE" ? true : false;
-      item.core_required = item.core_required === "TRUE" ? true : false;
       item.core_visible = item.core_visible === "TRUE" ? true : false;
       return item;
     }).filter(row => row !== null);
