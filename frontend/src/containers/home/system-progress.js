878f88582da03499683e5323f4af11047f5021d6
wildmolasses <changes@gmail.com>
2019-08-20 01:20:37 +0200

    pausing here after working index progress chart

---
+++ frontend/src/containers/home/system-progress.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { connect } from 'react-redux'
+// import {withPolling} from "../../modules/withPolling"
+
+
+const SystemProgress = (props) => {
+    return (
+        <div className="system-progress">
+            <h1>Index Progress</h1>
+            <div>
+            Size: {props.systemData.caches !== undefined && 
+                (props.systemData.caches[0].sizeInBytes * 1e-9).toFixed(2)} GB
+            
+                </div>
+            {props.chainStatus.finalized !== undefined &&
+                <SystemProgressChart {...props}/>
+            }
+        </div>
+    )
+}
+
+const SystemProgressChart = (props) => {
+    let client = props.chainStatus.client;
+    let clientHead = props.chainStatus.client + 100;
+    let ripe = props.chainStatus.ripe;
+    let unripe = props.chainStatus.unripe;
+    let finalized = props.chainStatus.finalized;
+
+    console.log(client, clientHead, ripe, unripe, finalized)
+
+    let bars = {}
+    bars.finalized = {pct: Math.floor((finalized) / clientHead * 100), color: "#009900"}
+    bars.ripe = {pct: Math.floor((ripe - finalized) / clientHead * 100), color: "#55AA55"}
+    bars.unripe = {pct: Math.floor((unripe - ripe) / clientHead * 100), color: "#99AA99"}
+    bars.unsynced = {pct: Math.floor((client - ripe) / clientHead * 100), color: "#DDDDDD"}
+
+    let order = ["finalized", "ripe", "unripe", "unsynced"]
+
+    console.log(bars);
+    console.log(props.chainStatus);
+    return (
+        <div>
+            <div className="progress-bar stripes">
+            {/* <span className="red" style={{width: syncPct}}></span> */}
+            {order.map(item => 
+                <span style={{width: bars[item].pct + '%', backgroundColor: bars[item].color}}></span>
+            )}
+        </div>
+        </div>
+    )
+}
+
+const mapStateToProps = ({ indexData, chainStatus, systemStatus }) => (
+    {
+        systemData: systemStatus.systemData,
+        chainStatus: chainStatus.chainStatus,
+        indexData: indexData.data,
+        isLoading: indexData.isLoading
+    }
+)
+
+export default connect(
+  mapStateToProps,
+)(SystemProgress)
--- frontend/src/modules/index.js
+++ frontend/src/modules/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import systemStatus from './systemStatus'
 import chainStatus from './chainStatus'
 import monitorStatus from './monitorStatus'
+import indexData from './indexData'
 import settingsManager from './settingsManager'
 
 export default combineReducers({
   systemStatus,
   chainStatus,
   monitorStatus,
+  indexData,
   settingsManager
 })
