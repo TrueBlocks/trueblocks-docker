ab044abaa536e4678d747ee78b0a286f3cdb0c19
wildmolasses <changes@gmail.com>
2019-08-20 02:32:45 +0200

    include build for api

---
+++ api/build/precache-manifest.153e23c9ecab75cc7e2fd50312e42804.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "974679dc6e460d15582b3699b83f3dd8",
+    "url": "/static/media/trash-alt.974679dc.svg"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "88b7f06163566f121e4f",
+    "url": "/static/js/main.dd65c130.chunk.js"
+  },
+  {
+    "revision": "bdd12b984df07ed53fc7",
+    "url": "/static/js/2.4b5f98b6.chunk.js"
+  },
+  {
+    "revision": "88b7f06163566f121e4f",
+    "url": "/static/css/main.be7ba3e0.chunk.css"
+  },
+  {
+    "revision": "bdd12b984df07ed53fc7",
+    "url": "/static/css/2.e801e158.chunk.css"
+  },
+  {
+    "revision": "b2ee1321d7ff2e1718769f5d447c6120",
+    "url": "/index.html"
+  }
+];
---
+++ api/build/precache-manifest.d5c057c97b0464eb39f2d1d0172d014b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "974679dc6e460d15582b3699b83f3dd8",
+    "url": "/static/media/trash-alt.974679dc.svg"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "423911f07cad56a52933",
+    "url": "/static/js/main.2df854a5.chunk.js"
+  },
+  {
+    "revision": "bdd12b984df07ed53fc7",
+    "url": "/static/js/2.4b5f98b6.chunk.js"
+  },
+  {
+    "revision": "423911f07cad56a52933",
+    "url": "/static/css/main.be7ba3e0.chunk.css"
+  },
+  {
+    "revision": "bdd12b984df07ed53fc7",
+    "url": "/static/css/2.e801e158.chunk.css"
+  },
+  {
+    "revision": "94036f5847f5d0dfe52889d614c009d2",
+    "url": "/index.html"
+  }
+];
