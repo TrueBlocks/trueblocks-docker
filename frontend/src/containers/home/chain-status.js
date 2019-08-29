e2a46e5aea6ece24ff1cc81f0ad8beceab56d39c
Thomas Jay Rush <jrush@quickblocks.io>
2019-08-29 02:25:57 -0400

    Fixing warnings

--- frontend/src/containers/home/chain-status.js
+++ frontend/src/containers/home/chain-status.js
@@ -1,4 +1,4 @@
-import React from 'react'
+//import React from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import {getChainStatus} from '../../modules/chainStatus'
