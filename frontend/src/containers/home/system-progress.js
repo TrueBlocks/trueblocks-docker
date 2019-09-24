2426e3fda69438ffdb874ce48c7e54bd0d969f2c
wildmolasses <changes@gmail.com>
2019-09-24 17:13:05 -0400

    css and cleaning up

--- frontend/src/containers/home/system-progress.js
+++ frontend/src/containers/home/system-progress.js
@@ -3,17 +3,30 @@ import { connect } from 'react-redux'
 import { humanFileSize } from '../../helpers/filesize'
 
 const SystemProgress = (props) => {
+    const ready = props.systemData.caches !== undefined && props.chainStatus.finalized !== undefined
+    let container
+    switch (ready) {
+        case true:
+            container = (
+                <div>
+                    <div>
+                        Total Size: {humanFileSize(props.systemData.caches[0].sizeInBytes)}
+                    </div>
+                    <div>
+                        Number of index files: {props.systemData.caches[0].nFiles}
+                    </div>
+                        <SystemProgressChart {...props} />
+                    }
+                </div>
+            )
+            break;
+        default:
+            container = <div>Loading...</div>
+    }
     return (
         <div className="system-progress">
             <h1>Index Progress</h1>
-            <div>
-                Size: {props.systemData.caches !== undefined &&
-                    (humanFileSize(props.systemData.caches[0].sizeInBytes))}
-
-            </div>
-            {props.chainStatus.finalized !== undefined &&
-                <SystemProgressChart {...props} />
-            }
+            {container}
         </div>
     )
 }
@@ -47,24 +60,24 @@ const SystemProgressChart = (props) => {
             {[...Array(rows).keys()].map((row, rowI) => {
                 return (
                     <React.Fragment>
-                    <div className='x-axis grid'>{row * 1e6}</div>
-                    {[...Array(cols).keys()].map((col, colI) => {
-                        let indexClass
-                        if(finalized >= row*1e6 + (col+1)*1e5) {
-                            indexClass = 'finalized'
-                        } else if(finalized >= row*1e6 + col*1e5) {
-                            indexClass = 'in-progress'
-                        } else {
-                            indexClass = 'inactive'
-                        }
-                        return (
-                        <div className='grid'><div className={`filling ${indexClass}`}>{indexClass === 'finalized' && '✔'}</div></div>
-                        )
-                    })}
+                        <div className='x-axis grid'>{row * 1e6}</div>
+                        {[...Array(cols).keys()].map((col, colI) => {
+                            let indexClass
+                            if (finalized >= row * 1e6 + (col + 1) * 1e5) {
+                                indexClass = 'finalized'
+                            } else if (finalized >= row * 1e6 + col * 1e5) {
+                                indexClass = 'in-progress'
+                            } else {
+                                indexClass = 'inactive'
+                            }
+                            return (
+                                <div className='grid'><div className={`filling ${indexClass}`}>{indexClass === 'finalized' && '✔'}</div></div>
+                            )
+                        })}
                     </React.Fragment>
                 )
             })
-        }
+            }
         </div>
     )
 
@@ -74,34 +87,6 @@ const SystemProgressChart = (props) => {
         </div>
     )
 }
-
-// const SystemProgressChart = (props) => {
-//     const client = props.chainStatus.client;
-//     const clientHead = props.chainStatus.client + 100;
-//     const ripe = props.chainStatus.ripe;
-//     const unripe = props.chainStatus.unripe;
-//     const finalized = props.chainStatus.finalized;
-
-//     console.log(client, clientHead, ripe, unripe, finalized)
-
-//     let bars = {}
-//     bars.finalized = {pct: Math.floor((finalized) / clientHead * 100), color: "#009900"}
-//     bars.ripe = {pct: Math.floor((ripe - finalized) / clientHead * 100), color: "#55AA55"}
-//     bars.unripe = {pct: Math.floor((unripe - ripe) / clientHead * 100), color: "#99AA99"}
-//     bars.unsynced = {pct: Math.floor((client - ripe) / clientHead * 100), color: "#DDDDDD"}
-
-//     let order = ["finalized", "ripe", "unripe", "unsynced"]
-
-//     return (
-//         <div>
-//             <div className="progress-bar stripes">
-//                 {order.map(item => 
-//                     <span style={{width: bars[item].pct + '%', backgroundColor: bars[item].color}}></span>
-//                 )}
-//             </div>
-//         </div>
-//     )
-// }
 
 const mapStateToProps = ({ indexData, systemStatus }) => (
     {
