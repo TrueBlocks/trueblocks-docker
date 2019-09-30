76e94d27edcb244671111ffd147c0ea2a7e26969
wildmolasses <changes@gmail.com>
2019-09-30 15:14:23 -0400

    more moving

--- frontend/src/containers/home/index-progress.js
+++ frontend/src/containers/home/index-progress.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { humanFileSize } from '../../helpers/filesize'
 import { getIndexData } from '../../modules/getIndexData'
-import SystemProgressChart from './system-progress-chart'
 
 const IndexProgress = (props) => {
     const ready = props.systemData.caches !== undefined && props.chainStatus.finalized !== undefined
@@ -38,6 +37,58 @@ const IndexProgress = (props) => {
                 You can set some options about this in the Settings tab.
             </p>
             {readyContainer}
+        </div>
+    )
+}
+
+const SystemProgressChart = (props) => {
+
+    // const ripe = props.chainStatus.ripe;
+    const unripe = props.chainStatus.unripe;
+    const finalized = props.chainStatus.finalized;
+    const clientHead = (props.chainStatus.client == "n/a" ? unripe : props.chainStatus.client);
+
+    const rows = Math.ceil(clientHead / 1e6)
+    const cols = 10
+
+    const chart = (
+        <div className='chart-container'>
+            <div className='y-axis grid'></div>
+            {[...Array(cols).keys()].map((col, colI) => {
+                return <div className='y-axis grid' key={`x${col}`}>{col * 1e5}</div>
+            })}
+            {[...Array(rows).keys()].map((row, rowI) => {
+                return (
+                    <React.Fragment key={`x${row}`}>
+                        <div className='x-axis grid'>{row * 1e6}</div>
+                        {[...Array(cols).keys()].map((col, colI) => {
+                            let indexClass
+                            if (finalized >= row * 1e6 + (col + 1) * 1e5) {
+                                indexClass = 'finalized'
+                            } else if (finalized >= row * 1e6 + col * 1e5) {
+                                indexClass = 'in-progress'
+                            } else {
+                                indexClass = 'inactive'
+                            }
+                            return (
+                                <div className='grid' key={`x${row}${col}`}>
+                                    <div className={`filling ${indexClass}`}>
+                                        {indexClass === 'finalized' && '✔'}
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </React.Fragment>
+                )
+            })
+            }
+        </div>
+    )
+
+    return (
+        <div>
+            {/* <ZoomOnIndex {...props} start={26e5} n={1e5}/> */}
+            {chart}
         </div>
     )
 }
