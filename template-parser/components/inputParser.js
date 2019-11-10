229041f73a6b2254dda36837c6d501b98d2c0055
Thomas Jay Rush <jrush@quickblocks.io>
2019-11-10 18:32:02 -0500

    Fixing creation of docs

--- template-parser/components/inputParser.js
+++ template-parser/components/inputParser.js
@@ -7,10 +7,10 @@ const utils = require('./utils');
 const inputParser = async (data) => {
   let lines = parse(data, { delimiter: ',', columns: true, skip_lines_with_error: true });
   let parsedLines = lines.map(item => {
-    item.is_required = item.is_required === "TRUE" ? true : false;
-    item.customizeable = item.customizeable === "TRUE" ? true : false;
-    item.docs_visible = item.docs_visible === "TRUE" ? true : false;
-    item.core_visible = item.core_visible === "TRUE" ? true : false;
+    item.is_required   = (item.is_required   === "TRUE" || item.is_required     === "true") ? true : false;
+    item.customizeable = (item.customizeable === "TRUE" || item.is_customizable === "true") ? true : false;
+    item.docs_visible  = (item.docs_visible  === "TRUE" || item.docs_visible    === "true") ? true : false;
+    item.core_visible  = (item.core_visible  === "TRUE" || item.core_visible    === "true") ? true : false;
     return item;
   }).filter(row => row !== null);
 
