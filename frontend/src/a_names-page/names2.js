82d5d523dcbdb113ae179b9225ba5f6e66a3d942
Thomas Jay Rush <jrush@quickblocks.io>
2019-12-12 10:26:18 -0500

    Updating names pages with css grids

--- frontend/src/a_names-page/names2.js
+++ frontend/src/a_names-page/names2.js
@@ -19,7 +19,7 @@ const NamesInner2 = (props) => {
           These names may be shared anonomously with the TrueBlocks community."
         />
         <div className="inner-panel">
-          <h4>Named Addresses</h4>
+          <h4>Summary</h4>
           <div className="name-container">
             <div className="grid2 blank1"> </div>
             <div className="grid2"> </div>
@@ -29,44 +29,78 @@ const NamesInner2 = (props) => {
             <div className="grid2 x-axis2">Community</div>
             <div className="grid2 blank3"> </div>
             <div className="grid2 blank1"> </div>
-            <div className="grid2 y-axis2">Expand >></div>
-            <div className="grid2 x-blank">500</div>
-            <div className="grid2 x-blank">23</div>
-            <div className="grid2 x-blank">12</div>
-            <div className="grid2 x-blank">12</div>
+            <div className="grid2 y-axis2">Addresses</div>
+            <div className="grid2 x-blank">40</div>
+            <div className="grid2 x-blank">991</div>
+            <div className="grid2 x-blank">8,894</div>
+            <div className="grid2 x-blank">916</div>
             <div className="grid2 blank3"> </div>
           </div>
-          <h4>Named Functions / Events</h4>
           <div className="name-container">
             <div className="grid2 blank1"> </div>
             <div className="grid2"> </div>
-            <div className="grid2 x-axis2">From Monitors</div>
-            <div className="grid2 x-axis2">Known Contracts</div>
-            <div className="grid2 x-axis2">Generated</div>
+            <div className="grid2 x-axis3">From Monitors</div>
+            <div className="grid2 x-axis3">Known Contracts</div>
+            <div className="grid2 x-axis3">Generated</div>
             <div className="grid2 blank2"> </div>
             <div className="grid2 blank1"> </div>
-            <div className="grid2 y-axis2">Expand >></div>
-            <div className="grid2 x-blank">2</div>
-            <div className="grid2 x-blank">232</div>
+            <div className="grid2 y-axis2">Functions / Events</div>
+            <div className="grid2 x-blank">406</div>
+            <div className="grid2 x-blank">1,559</div>
             <div className="grid2 x-blank">1,212,121</div>
             <div className="grid2 blank2"> </div>
           </div>
-          <h4>Named Blocks</h4>
           <div className="name-container">
             <div className="grid2 blank1"> </div>
             <div className="grid2"> </div>
-            <div className="grid2 x-axis2">Custom Names</div>
-            <div className="grid2 x-axis2">Known Blocks</div>
-            <div className="grid2 x-axis2">Generated</div>
+            <div className="grid2 x-axis3">Custom Names</div>
+            <div className="grid2 x-axis3">Known Blocks</div>
+            <div className="grid2 x-axis3">Generated</div>
             <div className="grid2 blank2"> </div>
             <div className="grid2 blank1"> </div>
-            <div className="grid2 y-axis2">Expand >></div>
-            <div className="grid2 x-blank">12</div>
-            <div className="grid2 x-blank">1</div>
-            <div className="grid2 x-blank">1</div>
+            <div className="grid2 y-axis2">Blocks</div>
+            <div className="grid2 x-blank">0</div>
+            <div className="grid2 x-blank">26</div>
+            <div className="grid2 x-blank">501</div>
+            <div className="grid2 blank2"> </div>
+          </div>
+          <div className="name-container">
+            <div className="grid2 blank1"> </div>
+            <div className="grid2"> </div>
+            <div className="grid2 x-axis3">Groups</div>
+            <div className="grid2 x-axis3">Sub Groups</div>
+            <div className="grid2 x-axis3">Other</div>
+            <div className="grid2 x-axis2">Community</div>
+            <div className="grid2 blank3"> </div>
+            <div className="grid2 blank1"> </div>
+            <div className="grid2 y-axis2">Other</div>
+            <div className="grid2 x-blank">10</div>
+            <div className="grid2 x-blank">7</div>
+            <div className="grid2 x-blank">0</div>
+            <div className="grid2 x-blank">0</div>
             <div className="grid2 blank2"> </div>
           </div>
         </div>
+        <div class="wrapper">
+          <div class="box a">A</div>
+          <div class="box b">B</div>
+          <div class="box c">C</div>
+          <div class="box d">D</div>
+          <div class="box e">E</div>
+          <div class="box f">F</div>
+          <div class="box g">G</div>
+          <div class="box h">H</div>
+          <div class="box i">I</div>
+          <div class="box j">J</div>
+          <div class="box k">K</div>
+          <div class="box l">L</div>
+          <div class="box g">G</div>
+          <div class="box h">H</div>
+          <div class="box i">I</div>
+          <div class="box j">J</div>
+          <div class="box k">K</div>
+          <div class="box l">L</div>
+        </div>{' '}
       </div>
     </div>
   );
