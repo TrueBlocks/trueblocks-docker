395227b7e11cfda8ecacaa40e76b552916d77a71
Thomas Jay Rush <jrush@quickblocks.io>
2020-01-30 22:57:22 -0500

    Small cleanup

--- frontend/src/.eslintrc.js
+++ frontend/src/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-    "extends": [ "react-app" ],
-    "rules" : {
-        "indent": ["error", 2, { "SwitchCase": 1, "ignoreComments": true }]
-    }
-}
+  extends: ['react-app'],
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1, ignoreComments: true }]
+  }
+};
