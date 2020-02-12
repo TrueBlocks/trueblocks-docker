e065ffd2d1686f48fd3a5b68c5f04fc4c73c70e9
Thomas Jay Rush <jrush@quickblocks.io>
2020-02-12 12:55:08 -0500

    Adding skins folder

---
+++ frontend/src/skins/fly50.js
@@ -0,0 +1,13 @@
+const fly50 = {
+  colorBorderPrimary: '#687864',
+  colorTableBorder: '#687864',
+  colorBackgroundPrimary: '#687864',
+  colorBackgroundSecondary: '#31708e',
+  colorTextPrimary: 'white',
+  colorTableText: '#5085a5',
+  colorTableHeaderBg: '#8fc1e3',
+  colorTableBodyBg: '#8fc1e3',
+  colorHover: '#f7f9fb',
+  dataTableWidth: '98%'
+};
+export default fly50;
---
+++ frontend/src/skins/love_apart.js
@@ -0,0 +1,13 @@
+const love_apart = {
+  colorBorderPrimary: '#806543',
+  colorTableBorder: '#806543',
+  colorBackgroundPrimary: '#806543',
+  colorBackgroundSecondary: '#33266e',
+  colorTextPrimary: 'white',
+  colorTableText: 'goldenrod',
+  colorTableHeaderBg: '#111111',
+  colorTableBodyBg: '#111111',
+  colorHover: '#a6607c',
+  dataTableWidth: '98%'
+};
+export default love_apart;
