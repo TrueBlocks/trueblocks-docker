8bd3a651d4e037037a9d48058cdf849c9bfd3bd5
wildmolasses <changes@gmail.com>
2020-04-14 15:54:03 -0500

    remove template-parser stuff

--- template-parser/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#npm install
-node index.js -i ../../trueblocks-core/src/other/build_assets/option-master-list.csv
-node ./node_modules/aglio/bin/aglio.js -o ./output/docs.html -i ./output/apiary.generated.apib
-cp -f ./output/docs.html ../api/docs/index.html
-cp -f ./output/apiOptions.generated.json ../api/apiOptions.generated.json
-rm -fR ../frontend/public/docs
-cp -fR ../api/docs ../frontend/public ; echo
