ac5b4f0cb2600c783ede4f694288237071ad0928
Dawid Szlachta <dawids@fr24.com>
2021-06-11 22:19:20 +0200

    Create config file if it is missing

--- trueblocks.entrypoint.sh
+++ trueblocks.entrypoint.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+chifra --version
+
+CONFIG_FILE=/root/.local/share/trueblocks/trueBlocks.toml
+
 # write the RPC provider to quickBlocks.toml
-sed -i "s|rpcProvider =.*|rpcProvider = \"$RPC_PROVIDER\"|" /root/.quickBlocks/quickBlocks.toml
+if grep -q rpcProvider "$CONFIG_FILE"; then
+    sed -i "s|rpcProvider =.*|rpcProvider = \"$RPC_PROVIDER\"|" $CONFIG_FILE
+else
+    echo "Writing RPC provider for the first run"
+    echo "rpcProvider = \"$RPC_PROVIDER\"" >> $CONFIG_FILE
+fi
 
 export DOCKER_MODE=true
 # chifra scrape --sleep 14 --daemon &
-# forever /root/trueblocks-explorer/api/server.js 80
 chifra serve
