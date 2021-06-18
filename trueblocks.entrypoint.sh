042cbdd496a653443ca8cbdd2d693dfabb1c9932
Dawid Szlachta <dawids@fr24.com>
2021-06-18 16:21:02 +0200

    Create blockScrape.toml when component starts

--- trueblocks.entrypoint.sh
+++ trueblocks.entrypoint.sh
@@ -3,6 +3,7 @@
 chifra --version
 
 CONFIG_FILE=/root/.local/share/trueblocks/trueBlocks.toml
+BLOCKSCRAPE_FILE=/root/.local/share/trueblocks/blockScrape.toml
 
 # write the RPC provider to quickBlocks.toml
 if grep -q rpcProvider "$CONFIG_FILE"; then
@@ -12,6 +13,14 @@ else
     echo "rpcProvider = \"$RPC_PROVIDER\"" >> $CONFIG_FILE
 fi
 
+# create blockScrape.toml as a workaround for https://github.com/TrueBlocks/trueblocks-core/issues/1577
+# (if this file is missing, RPC returns empty response)
+if [ ! -f "$BLOCKSCRAPE_FILE" ]; then
+    echo "[requires]" >> $BLOCKSCRAPE_FILE
+    echo "tracing=false" >> $BLOCKSCRAPE_FILE
+fi
+
 export DOCKER_MODE=true
-# chifra scrape --sleep 14 --daemon &
-chifra serve
+
+# the host has to be set to 0.0.0.0, otherwise Docker will refuse connections
+chifra serve --port 0.0.0.0:8080
