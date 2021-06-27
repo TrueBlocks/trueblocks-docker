5897458ddaaeddb0ac123873fd3f0909f8f6fb9f
Dawid Szlachta <dawids@fr24.com>
2021-06-27 20:09:51 +0200

    Added etherscan_key to config and chifra init to the entrypoint

--- trueblocks.entrypoint.sh
+++ trueblocks.entrypoint.sh
@@ -8,9 +8,11 @@ BLOCKSCRAPE_FILE=/root/.local/share/trueblocks/blockScrape.toml
 # write the RPC provider to quickBlocks.toml
 if grep -q rpcProvider "$CONFIG_FILE"; then
     sed -i "s|rpcProvider =.*|rpcProvider = \"$RPC_PROVIDER\"|" $CONFIG_FILE
+    sed -i "s|etherscan_key =.*|etherscan_key = \"$ETHERSCAN_KEY\"|" $CONFIG_FILE
 else
     echo "Writing RPC provider for the first run"
     echo "rpcProvider = \"$RPC_PROVIDER\"" >> $CONFIG_FILE
+    echo "etherscan_key = \"$ETHERSCAN_KEY\"" >> $CONFIG_FILE
 fi
 
 # create blockScrape.toml as a workaround for https://github.com/TrueBlocks/trueblocks-core/issues/1577
@@ -21,6 +23,8 @@ if [ ! -f "$BLOCKSCRAPE_FILE" ]; then
 fi
 
 export DOCKER_MODE=true
+
+chifra init&
 
 # the host has to be set to 0.0.0.0, otherwise Docker will refuse connections
 chifra serve --port 0.0.0.0:8080
