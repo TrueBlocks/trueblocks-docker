f02263c8723fc99bc14a1053dbd8b0fd0d475ebf
tjayrush <jrush@quickblocks.io>
2022-12-10 20:20:55 -0500

    Simply removes all comments to make the scripts / Dockerfiles easier to understand

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-# This script builds the images and pushes them to docker hub
-
-# tagged version
 VERSION=v0.44.0-beta
-
-# Add --no-cache to the build commands to create a fresh build
 
 docker build build/core --tag trueblocks/core:$VERSION
 docker push trueblocks/core:$VERSION
