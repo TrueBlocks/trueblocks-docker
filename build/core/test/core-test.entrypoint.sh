552ed0e6570d034d547c0d3ddcafe30d230266bc
tjayrush <jrush@quickblocks.io>
2022-12-10 20:20:55 -0500

    Simply removes all comments to make the scripts / Dockerfiles easier to understand

--- build/core/test/core-test.entrypoint.sh
+++ build/core/test/core-test.entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Locations of config files
 CONFIG_FILE=/root/.local/share/trueblocks/trueBlocks.toml
 
 echo "Environment:"
@@ -8,10 +7,6 @@ env
 
 echo "Testing..."
 
-# Because C++ code used by chifra status always returns success,
-# we will use anonymous pipe to grep stderr and check if there are
-# any strings containing "error". Grep with -c option will return
-# the count of matched strings, so we can use it as our exit code
 ERRORS=`chifra status --terse &> >(grep -ci 'error\|Quitting')`
 if [ "$ERRORS" -gt 0 ]
 then
