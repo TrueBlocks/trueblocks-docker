fe755e54c24e1349e1f59d16565bc70f1c191956
tjayrush <jrush@quickblocks.io>
2022-12-10 20:37:45 -0500

    Removes comments

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,6 +4,3 @@ VERSION=v0.44.0-beta
 
 docker build build/core --tag trueblocks/core:$VERSION
 docker push trueblocks/core:$VERSION
-
-# docker build build/monitors --tag trueblocks/monitor:$VERSION
-# docker push trueblocks/monitor:$VERSION
