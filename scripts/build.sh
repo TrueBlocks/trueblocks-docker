7104967f6cdec6f5c219854a24cd9f5e5a35e4e1
tjayrush <jrush@quickblocks.io>
2023-03-11 23:14:43 -0500

    Updates to version v0.60.0-beta

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-VERSION=v0.55.0-beta
+VERSION=v0.60.0-beta
 docker build . --tag trueblocks/core:$VERSION
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=v0.55.0-beta
+VERSION=v0.60.0-beta
 
 docker build . --tag trueblocks/core:$VERSION
 docker push trueblocks/core:$VERSION
